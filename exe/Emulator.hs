85a766691452bdfc3a685d5e2ed2bedc8ad65fb2
waalge <waalgy@gmail.com>
2021-10-01 12:08:18 +0100

    Patched Emulator example. It runs but new case doesn't add anything Not sure the intention

--- exe/Emulator.hs
+++ exe/Emulator.hs
@@ -48,11 +48,11 @@ nft = AssetClass (nftCurrency, nftName)
 
 main :: IO ()
 main = do
-    runEmulatorTraceIO' def emCfg scenario1
+    -- runEmulatorTraceIO' def emCfg scenario1
     runEmulatorTraceIO' def emCfg scenario2
 
 emCfg :: EmulatorConfig
-emCfg = EmulatorConfig (Left $ Map.fromList [(Wallet w, v) | w <- [1 .. 2]]) def def
+emCfg = EmulatorConfig (Left $ Map.fromList [(knownWallet w, v) | w <- [1 .. 2]]) def def
     where
         v = Ada.lovelaceValueOf 1000_000_000 <> assetClassValue nft 1
 
@@ -86,9 +86,10 @@ scenario2 = do
             , fractions = 10
             , fractionTokenName = tokenName "Frac"
             }
-    -- callEndpoint @"1-lockNFT" h1 nft
+    callEndpoint @"1-lockNFT" h1 nft
     callEndpoint @"2-fractionNFT" h1 toFraction
+
     void $ Emulator.waitNSlots 1
 
-    callEndpoint @"3-returnNFT" h1 nft
+    -- callEndpoint @"3-returnNFT" h1 nft
     void $ Emulator.waitNSlots 1
